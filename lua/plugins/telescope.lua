85ab38b9ab7636932cd305d2ec2a0e8ebf591728
James Mattner <james.mattner@health.telstra.com>
2024-08-20 12:43:24 +0800

    refactor functions in telescope config

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,37 +1,42 @@
 return {
-    'nvim-telescope/telescope.nvim',
-    tag = '0.1.8',
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.8",
     dependencies = {
-        'nvim-lua/plenary.nvim'
+        "nvim-lua/plenary.nvim"
     },
-    keys = {
-        { "<leader>pf", (require("telescope.builtin").find_files), desc = "Search Files" },
-        {
-            "<leader>pws",
-            function()
-                local word = vim.fn.expand("<cword>")
-                require("telescope.builtin").grep_string({ search = word })
-            end,
-            desc = "Search Word"
-        },
-        {
-            "<leader>pWs",
-            function()
-                local word = vim.fn.expand("<cWORD>")
-                require("telescope.builtin").grep_string({ search = word })
-            end,
-            desc = "Search Word (outer)"
-        },
-        {
-            "<leader>ps",
-            function()
-                require("telescope.builtin").grep_string({ search = vim.fn.input("Grep > ") })
-            end,
-            desc = "Search Text"
-        },
-        { "<leader>pt", (require("telescope.builtin").colorscheme), desc = "Search Themes" },
-        { "<leader>pg", (require("telescope.builtin").git_commits), desc = "Search Commits" },
-        { "<leader>vh", (require("telescope.builtin").help_tags),  desc = "View Help" }
-    },
+    keys = function()
+        local builtin = require("telescope.builtin")
+        return {
+            {
+                "<leader>pf", builtin.find_files, desc = "Search Files"
+            },
+            {
+                "<leader>pws",
+                function()
+                    local word = vim.fn.expand("<cword>")
+                    builtin.grep_string({ search = word })
+                end,
+                desc = "Search Word"
+            },
+            {
+                "<leader>pWs",
+                function()
+                    local word = vim.fn.expand("<cWORD>")
+                    builtin.grep_string({ search = word })
+                end,
+                desc = "Search Word (outer)"
+            },
+            {
+                "<leader>ps",
+                function()
+                    builtin.grep_string({ search = vim.fn.input("Grep > ") })
+                end,
+                desc = "Search Text"
+            },
+            { "<leader>pt", builtin.colorscheme, desc = "Search Themes" },
+            { "<leader>pg", builtin.git_commits, desc = "Search Commits" },
+            { "<leader>vh", builtin.help_tags,   desc = "View Help" }
+        }
+    end,
     opts = {}
 }
