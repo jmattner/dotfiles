f013534e38ec872b9a9722659a95303d9bef97f7
James Mattner <james.mattner@gmail.com>
2024-08-31 22:39:21 +0800

    added some basic extended keymaps to fugitive

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,6 +1,15 @@
+vim.api.nvim_create_autocmd({ "User" }, {
+    pattern = "FugitiveIndex",
+    callback = function()
+        vim.keymap.set("n", "<leader>gk", "<cmd>Git push<cr>", { buffer = true })
+        vim.keymap.set("n", "<leader>gj", "<cmd>Git pull<cr>", { buffer = true })
+    end,
+})
+
 return {
     "tpope/vim-fugitive",
     keys = {
-        { "<leader>gs", vim.cmd.Git, desc = "Git Status" }
-    }
+        { "<leader>gs", vim.cmd.Git, desc = "Git Status" },
+    },
+    opts = {},
 }
