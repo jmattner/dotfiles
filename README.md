807edf77245bbc87bcfca34deea1b5b14b9705ad
James Mattner <james.mattner@gmail.com>
2024-09-01 14:32:52 +0800

    add telescope file explorer and file path status bar

--- README.md
+++ README.md
@@ -16,12 +16,11 @@ documenting things here because i'm stupid and i forget
 
 ## TODO
 
-- look into winbar.lua
-- add git bindings shortcuts for workflow
 - workspaces / projects on dashboard
 - session persistence on close
 - sessions on dashboard
 - get DAP working
 - tiny-code-action (code action telescope support)
+- make git bindings more context aware + add more
 - git commit viewing
 - align status bar colors with colorscheme
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,70 +1,93 @@
 return {
-    "nvim-telescope/telescope.nvim",
-    tag = "0.1.8",
-    dependencies = {
-        "nvim-lua/plenary.nvim"
-    },
-    keys = function()
-        local builtin = require("telescope.builtin")
-        return {
-            { "<leader>pp", builtin.builtin,    desc = "Pickers" },
-            { "<leader>pf", builtin.find_files, desc = "Files" },
-            {
-                "<leader>pws",
-                function()
-                    local word = vim.fn.expand("<cword>")
-                    builtin.grep_string({ search = word })
-                end,
-                desc = "Word Search"
-            },
-            {
-                "<leader>pWs",
-                function()
-                    local word = vim.fn.expand("<cWORD>")
-                    builtin.grep_string({ search = word })
-                end,
-                desc = "Word (outer) Search"
-            },
-            {
-                "<leader>ps",
-                function()
-                    builtin.grep_string({ search = vim.fn.input("Grep > ") })
-                end,
-                desc = "Text"
-            },
-            { "<leader>pt", builtin.colorscheme,          desc = "Themes" },
-            { "<leader>pg", builtin.git_commits,          desc = "Commits" },
-            { "<leader>ph", builtin.help_tags,            desc = "Help" },
+    {
+        "nvim-telescope/telescope.nvim",
+        tag = "0.1.8",
+        dependencies = {
+            "nvim-lua/plenary.nvim"
+        },
+        keys = function()
+            local builtin = require("telescope.builtin")
+            return {
+                { "<leader>pp", builtin.builtin,    desc = "Pickers" },
+                { "<leader>pf", builtin.find_files, desc = "Files" },
+                {
+                    "<leader>pws",
+                    function()
+                        local word = vim.fn.expand("<cword>")
+                        builtin.grep_string({ search = word })
+                    end,
+                    desc = "Word Search"
+                },
+                {
+                    "<leader>pWs",
+                    function()
+                        local word = vim.fn.expand("<cWORD>")
+                        builtin.grep_string({ search = word })
+                    end,
+                    desc = "Word (outer) Search"
+                },
+                {
+                    "<leader>ps",
+                    function()
+                        builtin.grep_string({ search = vim.fn.input("Grep > ") })
+                    end,
+                    desc = "Text"
+                },
+                { "<leader>pt", builtin.colorscheme,          desc = "Themes" },
+                { "<leader>pg", builtin.git_commits,          desc = "Commits" },
+                { "<leader>ph", builtin.help_tags,            desc = "Help" },
 
-            -- lsp things
-            { "<leader>pd", builtin.lsp_definitions,      desc = "Definition" },
-            { "<leader>pD", builtin.lsp_type_definitions, desc = "Type Definition" },
-            { "<leader>pr", builtin.lsp_references,       desc = "Reference" },
-            { "<leader>pi", builtin.lsp_implementations,  desc = "Implementation" },
-            { "<leader>pz", print("asdf"), desc = "Test" },
-        }
-    end,
-    opts = {
-        pickers = {
-            lsp_references = {
-                layout_strategy = "vertical",
-                layout_config = {
-                    width = 0.95,
-                    height = 0.95,
-                    anchor = "S",
+                -- lsp things
+                { "<leader>pd", builtin.lsp_definitions,      desc = "Definition" },
+                { "<leader>pD", builtin.lsp_type_definitions, desc = "Type Definition" },
+                { "<leader>pr", builtin.lsp_references,       desc = "Reference" },
+                { "<leader>pi", builtin.lsp_implementations,  desc = "Implementation" },
+            }
+        end,
+        opts = {
+            pickers = {
+                lsp_references = {
+                    layout_strategy = "vertical",
+                    layout_config = {
+                        width = 0.95,
+                        height = 0.95,
+                        anchor = "S",
+                    },
+                    path_display = {
+                        shorten = 2
+                    },
+                    fname_width = 0.5,
+                    -- show_line = false,
                 },
-                path_display = {
-                    shorten = 2
+                diagnostics = {
+                    path_display = function(opts, path)
+                        local tail = require("telescope.utils").path_tail(path)
+                        return string.format("%s (%s)", tail, path)
+                    end,
                 },
-                fname_width = 0.5,
-                -- show_line = false,
-            },
-            diagnostics = {
-                path_display = function(opts, path)
-                    local tail = require("telescope.utils").path_tail(path)
-                    return string.format("%s (%s)", tail, path)
-                end,
             },
         },
     },
+    {
+        "nvim-telescope/telescope-file-browser.nvim",
+        dependencies = {
+            "nvim-telescope/telescope.nvim",
+            "nvim-lua/plenary.nvim"
+        },
+        keys = function()
+            local browser = require("telescope").extensions.file_browser
+            return {
+                {
+                    "<leader>pe",
+                    function()
+                        browser.file_browser({
+                            path = "%:p:h",
+                            select_buffer = true,
+                        })
+                    end,
+                    desc = "File Explorer"
+                },
+            }
+        end,
+    }
 }
