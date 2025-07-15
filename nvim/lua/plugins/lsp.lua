75ae638ca00fde37298343bfaf5324266734ca94
James Mattner <james.mattner@health.telstra.com>
2025-07-15 22:29:45 +0800

    updated lsp config to use mason-lspconfig again

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -10,6 +10,33 @@ return {
         },
     },
     {
+        "williamboman/mason-lspconfig.nvim",
+        dependencies = {
+            "mason-org/mason.nvim",
+            "neovim/nvim-lspconfig",
+        },
+        opts = {
+            automatic_enable = {
+                exclude = {
+                    "lua_ls", -- handled by lua_ls.lua
+                    "roslyn", -- handled by roslyn.nvim
+                },
+            },
+            ensure_installed = {
+                "docker_compose_language_service",
+                "dockerls",
+                "cssls",
+                "eslint",
+                "html",
+                "lua_ls",
+                "marksman",
+                -- "roslyn", not available currently via mason
+                "sqlls",
+                "ts_ls",
+            }
+        },
+    },
+    {
         "seblyng/roslyn.nvim",
         ft = "cs",
         opts = {},
